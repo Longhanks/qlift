7ee3479f4910aef105e6540a31d74831eb76c8d0
Andreas Schulz <andi.schulz@me.com>
2017-07-06 22:00:00 +0200

    Show mainwindow

--- Sources/main.swift
+++ Sources/main.swift
@@ -9,6 +9,11 @@ import CQlift
 
 var argc = CommandLine.argc
 let app = QApplication_ctor(&argc, CommandLine.unsafeArgv)
+let mw = QMainWindow_ctor_parent_default_flags_default();
+let label = QLabel_ctor_text_arg1_parent_arg2_flags_default("Hello, world!", mw)
+QLabel_setAlignment(label, 132)
+QMainWindow_setCentralWidget(mw, label)
+QMainWindow_show(mw)
 let returnValue = QApplication_exec(app)
 exit(returnValue)
 
