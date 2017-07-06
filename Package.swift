071ed7ee181b7ef1bcc17bed519dd34f52d4b49f
Andreas Schulz <andi.schulz@me.com>
2017-07-06 20:30:00 +0200

    First working demo

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "Qlift",
+    dependencies: [
+        .Package(url: "https://github.com/Longhanks/CQlift", majorVersion: 1)
+    ]
+)
---
+++ Sources/main.swift
@@ -0,0 +1,4 @@
+import CQlift
+
+let app = QApplication_ctor(CommandLine.argc, CommandLine.unsafeArgv)
+print("app is: \(String(describing: type(of: app)))")
