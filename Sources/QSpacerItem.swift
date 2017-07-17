68edaecec0160cfe347206cd92cd8eda75cf2a07
Andreas Schulz <andi.schulz@me.com>
2017-07-17 23:47:00 +0200

    QSpacerItem: Rename argument labels

--- Sources/QSpacerItem.swift
+++ Sources/QSpacerItem.swift
@@ -5,8 +5,8 @@ open class QSpacerItem: QLayoutItem {
     public var ptr: UnsafeMutableRawPointer?
     public var needsDelete = true
 
-    public init(width: Int32, height: Int32, hPolicy: QSizePolicy.Policy = .Minimum, vPolicy: QSizePolicy.Policy = .Minimum) {
-        self.ptr = QSpacerItem_new(width, height, hPolicy.rawValue, vPolicy.rawValue)
+    public init(width: Int32, height: Int32, horizontalPolicy: QSizePolicy.Policy = .Minimum, verticalPolicy: QSizePolicy.Policy = .Minimum) {
+        self.ptr = QSpacerItem_new(width, height, horizontalPolicy.rawValue, verticalPolicy.rawValue)
     }
 
     deinit {
