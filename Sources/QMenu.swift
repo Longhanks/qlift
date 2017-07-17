ea2515381840ebb38c026df31a1ea472811c556d
Andreas Schulz <andi.schulz@me.com>
2017-07-17 23:54:45 +0200

    QMenu: Introduce title/setTitle, menuAction()

--- Sources/QMenu.swift
+++ Sources/QMenu.swift
@@ -2,12 +2,25 @@ import CQlift
 
 
 open class QMenu: QWidget {
+    public var title: String {
+        get {
+            return String(cString: QMenu_title(self.ptr))
+        }
+        set(newTitle) {
+            QMenu_setTitle(self.ptr, newTitle)
+        }
+    }
+
     public init(title: String? = nil, parent: QWidget? = nil) {
         super.init(ptr: QMenu_new(title, parent?.ptr))
     }
 
     override init(ptr: UnsafeMutableRawPointer) {
         super.init(ptr: ptr)
+    }
+
+    public func menuAction() -> QAction {
+        return QAction(ptr: QMenu_menuAction(self.ptr))
     }
 
     deinit {
