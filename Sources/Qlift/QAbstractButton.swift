0e2d2248d55d0d9e4c0fe5cbce991180774efe08
Andreas Schulz <andi.schulz@me.com>
2017-07-20 20:15:00 +0200

    Context-capturing closure in QAbstractButton.connectClicked()

--- Sources/Qlift/QAbstractButton.swift
+++ Sources/Qlift/QAbstractButton.swift
@@ -2,6 +2,8 @@ import CQlift
 
 
 open class QAbstractButton: QWidget {
+    var clickedCallback: ((Bool) -> Void)?
+
     public var text: String {
         get {
             return String(cString: QAbstractButton_text(self.ptr))
@@ -15,12 +17,28 @@ open class QAbstractButton: QWidget {
         super.init(ptr: ptr, parent: parent)
     }
 
-    open func connectClicked(receiver: QObject? = nil, to slot: @escaping @convention(c) (Int32) -> Void) {
+    open func connectClicked(receiver: QObject? = nil, to slot: @escaping ((Bool) -> Void)) {
         var object: QObject = self
         if receiver != nil {
             object = receiver!
         }
-        QAbstractButton_clicked_connect(self.ptr, object.ptr, slot)
+
+        self.clickedCallback = slot
+
+        let functor: @convention(c) (UnsafeMutableRawPointer?, Int32) -> Void = { raw, checked in
+            if raw != nil {
+                let _self = Unmanaged<QAbstractButton>.fromOpaque(raw!).takeUnretainedValue()
+                if checked == 0 {
+                    _self.clickedCallback!(false)
+                } else {
+                    _self.clickedCallback!(true)
+                }
+            }
+        }
+
+        let rawSelf = Unmanaged.passUnretained(self).toOpaque()
+
+        QAbstractButton_clicked_connect(self.ptr, object.ptr, rawSelf, functor)
     }
 }
 
