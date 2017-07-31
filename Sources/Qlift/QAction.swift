d2640b68b837945764d3a13552ec510a13be4b6e
Andreas Schulz <andi.schulz@me.com>
2017-07-31 21:30:00 +0200

    Introduce QAction.connectTriggered

--- Sources/Qlift/QAction.swift
+++ Sources/Qlift/QAction.swift
@@ -2,6 +2,8 @@ import CQlift
 
 
 open class QAction: QObject {
+    var triggeredCallback: ((Bool) -> Void)?
+
     public var text: String {
         get {
             return String(cString: QAction_text(self.ptr))
@@ -27,6 +29,30 @@ open class QAction: QObject {
             }
             self.ptr = nil
         }
+    }
+
+    open func connectTriggered(receiver: QObject? = nil, to slot: @escaping ((Bool) -> Void)) {
+        var object: QObject = self
+        if receiver != nil {
+            object = receiver!
+        }
+
+        self.triggeredCallback = slot
+
+        let functor: @convention(c) (UnsafeMutableRawPointer?, Int32) -> Void = { raw, checked in
+            if raw != nil {
+                let _self = Unmanaged<QAction>.fromOpaque(raw!).takeUnretainedValue()
+                if checked == 0 {
+                    _self.triggeredCallback!(false)
+                } else {
+                    _self.triggeredCallback!(true)
+                }
+            }
+        }
+
+        let rawSelf = Unmanaged.passUnretained(self).toOpaque()
+
+        QAction_triggered_connect(self.ptr, object.ptr, rawSelf, functor)
     }
 }
 
