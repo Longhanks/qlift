464708ad37965f309110186df3b36ab9e091e81b
Andreas Schulz <andi.schulz@me.com>
2017-08-05 22:11:27 +0200

    Add QGroupBox

---
+++ Sources/Qlift/QGroupBox.swift
@@ -0,0 +1,37 @@
+import CQlift
+
+
+open class QGroupBox: QWidget {
+    public var alignment: Qt.Alignments = .AlignLeft {
+        didSet {
+            QGroupBox_setAlignment(self.ptr, alignment.rawValue)
+        }
+    }
+
+    public var title: String {
+        get {
+            return String(cString: QGroupBox_title(self.ptr))
+        }
+        set(newText) {
+            QGroupBox_setTitle(self.ptr, newText)
+        }
+    }
+
+    public init(title: String = "", parent: QWidget? = nil) {
+        super.init(ptr: QGroupBox_new(title, parent?.ptr), parent: parent)
+    }
+
+    override init(ptr: UnsafeMutableRawPointer, parent: QWidget? = nil) {
+        super.init(ptr: ptr, parent: parent)
+    }
+
+    deinit {
+        if self.ptr != nil {
+            if QObject_parent(self.ptr) == nil {
+                QGroupBox_delete(self.ptr)
+            }
+            self.ptr = nil
+        }
+    }
+}
+
