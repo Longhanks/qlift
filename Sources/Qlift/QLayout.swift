2f820f9d9f9bf616468cd8e8cf83d74d7a7bcb2f
Andreas Schulz <andi.schulz@me.com>
2017-08-14 22:35:11 +0200

    Introduce QLayout sizeConstraint

--- Sources/Qlift/QLayout.swift
+++ Sources/Qlift/QLayout.swift
@@ -1,7 +1,19 @@
 import CQlift
 
+
 public protocol QLayout: QLayoutItem {
     func add(item: QLayoutItem)
     func add(widget: QWidget)
+}
+
+extension QLayout {
+    public var sizeConstraint: QLayoutSizeConstraint {
+        get {
+            return QLayoutSizeConstraint(rawValue: QLayout_sizeConstraint(self.ptr))
+        }
+        set(newSizeConstraint) {
+            QLayout_setSizeConstraint(self.ptr, newSizeConstraint.rawValue)
+        }
+    }
 }
 
---
+++ Sources/Qlift/QLayoutSizeConstraint.swift
@@ -0,0 +1,15 @@
+public struct QLayoutSizeConstraint: OptionSet {
+    public let rawValue: Int32
+    
+    public init(rawValue: Int32) {
+        self.rawValue = rawValue
+    }
+
+    public static let SetDefaultConstraint = QLayoutSizeConstraint(rawValue: 0)
+    public static let SetNoConstraint = QLayoutSizeConstraint(rawValue: 1)
+    public static let SetMinimumSize = QLayoutSizeConstraint(rawValue: 2)
+    public static let SetFixedSize = QLayoutSizeConstraint(rawValue: 3)
+    public static let SetMaximumSize = QLayoutSizeConstraint(rawValue: 4)
+    public static let SetMinAndMaxSize = QLayoutSizeConstraint(rawValue: 5)
+}
+
