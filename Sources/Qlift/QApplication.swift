48ed63c5e73c3aa4e85fb4d5641a9fdc9703e85d
Andreas Schulz <andi.schulz@me.com>
2017-08-16 13:04:21 +0200

    QApplication desktop()

--- Sources/Qlift/QApplication.swift
+++ Sources/Qlift/QApplication.swift
@@ -2,11 +2,13 @@ import CQlift
 
 
 public class QApplication: QGuiApplication {
+    public static var desktop: QDesktopWidget!
     private var argc = CommandLine.argc
 
     public override init() {
         super.init(ptr: QApplication_new(&argc, CommandLine.unsafeArgv))
         QCoreApplication.instance = self
+        QApplication.desktop = QDesktopWidget(ptr: QApplication_desktop())
     }
 
     deinit {
