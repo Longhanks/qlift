ebb401f7f2cfead2f7081b8f92038d9ca05eec55
Andreas Schulz <andi.schulz@me.com>
2017-08-16 13:08:16 +0200

    QDesktopWidget: Fix access modifier

--- Sources/Qlift/QDesktopWidget.swift
+++ Sources/Qlift/QDesktopWidget.swift
@@ -6,7 +6,7 @@ public class QDesktopWidget: QWidget {
         super.init(ptr: ptr, parent: nil)
     }
 
-    func availableGeometry(for widget: QWidget) -> QRect {
+    public func availableGeometry(for widget: QWidget) -> QRect {
         let rectPtr: UnsafeMutableRawPointer = QDesktopWidget_availableGeometryWidget(self.ptr, widget.ptr)
         return QRect(ptr: rectPtr)
     }
