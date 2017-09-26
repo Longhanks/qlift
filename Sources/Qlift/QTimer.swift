61d44ef4be60f956be117d9718ab2086ec3787b3
Andreas Schulz <andi.schulz@me.com>
2017-09-26 21:04:22 +0200

    QTimer: Closure support

--- Sources/Qlift/QTimer.swift
+++ Sources/Qlift/QTimer.swift
@@ -1,9 +1,22 @@
 import CQlift
 
+private class ClosureBox {
+    public var closure: () -> Void
+    init(_ closure: @escaping () -> Void) {
+        self.closure = closure
+    }
+}
 
 open class QTimer: QObject {
-    public static func singleShot(msec: Int32, timerType: Qt.TimerType, handler: @escaping @convention(c) () -> Void) {
-        QTimer_singleShot(msec, timerType.rawValue, handler)
+    public static func singleShot(msec: Int32, timerType: Qt.TimerType, handler: @escaping () -> Void) {
+        let functor: @convention(c) (UnsafeMutableRawPointer?) -> Void = { raw in
+            if raw != nil {
+                let box = Unmanaged<ClosureBox>.fromOpaque(raw!).takeUnretainedValue()
+                box.closure()
+            }
+        }
+        let rawClosure = Unmanaged.passUnretained(ClosureBox(handler)).toOpaque()
+        QTimer_singleShot(msec, timerType.rawValue, rawClosure, functor)
     }
 }
 
