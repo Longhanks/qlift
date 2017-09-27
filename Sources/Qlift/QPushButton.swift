df972960a564add2b4e2b13195b10fa3782c5ab8
Andreas Schulz <andi.schulz@me.com>
2017-09-27 20:15:48 +0200

    QPushButton: mousePressEvent

--- Sources/Qlift/QPushButton.swift
+++ Sources/Qlift/QPushButton.swift
@@ -5,6 +5,17 @@ open class QPushButton: QAbstractButton {
     // Icon not supported at the moment
     public init(text: String = "", parent: QWidget? = nil) {
         super.init(ptr: QPushButton_new(nil, text, parent?.ptr), parent: parent)
+
+        let rawSelf = Unmanaged.passUnretained(self).toOpaque()
+
+        let functor: @convention(c) (UnsafeMutableRawPointer?, UnsafeMutableRawPointer?) -> Void = { context, mouseEvent in
+            if context != nil {
+                let _self = Unmanaged<QPushButton>.fromOpaque(context!).takeUnretainedValue()
+                _self.mousePressEvent(event: mouseEvent)
+            }
+        }
+
+        QPushButton_mousePressEvent_Override(self.ptr, rawSelf, functor)
     }
 
     override init(ptr: UnsafeMutableRawPointer, parent: QWidget? = nil) {
@@ -18,6 +29,10 @@ open class QPushButton: QAbstractButton {
             }
             self.ptr = nil
         }
+    }
+
+    open func mousePressEvent(event: UnsafeMutableRawPointer?) {
+        QPushButton_mousePressEvent(self.ptr, event)
     }
 }
 
