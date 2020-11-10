1912cb86d1b265bf952e1694e504da0293c5ab2b
Andreas Schulz <andi.schulz@me.com>
2020-11-10 20:19:59 +0100

    Remove buildscript.sh, build with swiftpm

--- Package.swift
+++ Package.swift
@@ -1,17 +1,35 @@
 // swift-tools-version:5.3
-
 import PackageDescription
-
 
 let package = Package(
     name: "Qlift",
     products: [
-        .executable(name: "qlift-uic", targets: ["qlift-uic"]),
-        .library(name: "Qlift", targets: ["Qlift"]),
+        .executable(
+            name: "qlift-uic",
+            targets: [
+                "qlift-uic"
+            ]
+        ),
+        .library(
+            name: "Qlift",
+            targets: [
+                "Qlift"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(
+            name: "CQlift",
+            url: "https://github.com/Longhanks/qlift-c-api",
+            .branch("master")
+        )
     ],
     targets: [
         .target(
-            name: "Qlift"
+            name: "Qlift",
+            dependencies: [
+                "CQlift"
+            ]
         ),
         .target(
             name: "qlift-uic"
