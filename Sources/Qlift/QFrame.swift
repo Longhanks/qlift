59fb5df618817394583b0f17059d019ff4e7a0fa
Andreas Schulz <andi.schulz@me.com>
2020-11-11 19:24:02 +0100

    QFrame: sizeHint & mousePressEvent APIs

--- Sources/Qlift/QFrame.swift
+++ Sources/Qlift/QFrame.swift
@@ -4,6 +4,22 @@ import CQlift
 open class QFrame: QWidget {
     public override init(parent: QWidget? = nil, flags: Qt.WindowFlags = .Widget) {
         super.init(ptr: QFrame_new(parent?.ptr, flags.rawValue), parent: parent)
+
+        let rawSelf = Unmanaged.passUnretained(self).toOpaque()
+
+        let functorSizeHint: @convention(c) (UnsafeMutableRawPointer?) -> UnsafeMutableRawPointer? = { context in
+            let _self = Unmanaged<QFrame>.fromOpaque(context!).takeUnretainedValue()
+            return Unmanaged.passUnretained(_self.sizeHint).toOpaque()
+        }
+
+        QWidget_sizeHint_Override(self.ptr, rawSelf, functorSizeHint)
+
+        let functorMousePressEvent: @convention(c) (UnsafeMutableRawPointer?, UnsafeMutableRawPointer?) -> Void = { context, mouseEvent in
+            let _self = Unmanaged<QFrame>.fromOpaque(context!).takeUnretainedValue()
+            _self.mousePressEvent(event: QMouseEvent(ptr: mouseEvent!))
+        }
+
+        QWidget_mousePressEvent_Override(self.ptr, rawSelf, functorMousePressEvent)
     }
 
     override init(ptr: UnsafeMutableRawPointer, parent: QWidget? = nil) {
@@ -19,4 +35,3 @@ open class QFrame: QWidget {
         }
     }
 }
-
