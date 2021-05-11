532c32866562a9536bfa2dccd0ebef9e0dece316
Dmitriy Borovikov <dmitry@borovikov.name>
2021-05-11 21:22:20 +0300

    QPushButton Icon support

--- Sources/Qlift/QPushButton.swift
+++ Sources/Qlift/QPushButton.swift
@@ -2,9 +2,21 @@ import CQlift
 
 
 open class QPushButton: QAbstractButton {
-    // Icon not supported at the moment
     public init(text: String = "", parent: QWidget? = nil) {
         super.init(ptr: QPushButton_new(nil, text, parent?.ptr), parent: parent)
+
+        let rawSelf = Unmanaged.passUnretained(self).toOpaque()
+
+        let functor: @convention(c) (UnsafeMutableRawPointer?, UnsafeMutableRawPointer?) -> Void = { context, mouseEvent in
+            let _self = Unmanaged<QPushButton>.fromOpaque(context!).takeUnretainedValue()
+            _self.mousePressEvent(event: QMouseEvent(ptr: mouseEvent!))
+        }
+
+        QPushButton_mousePressEvent_Override(self.ptr, rawSelf, functor)
+    }
+
+    public init(icon: QIcon, text: String = "", parent: QWidget? = nil) {
+        super.init(ptr: QPushButton_new(icon.ptr, text, parent?.ptr), parent: parent)
 
         let rawSelf = Unmanaged.passUnretained(self).toOpaque()
 
