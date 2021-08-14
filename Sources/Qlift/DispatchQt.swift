f6f812ed227a56f49c8a439b6c21b26a270fb90a
Dmitriy Borovikov <dmitry@borovikov.name>
2021-08-14 22:49:52 +0300

    bindMainRunloopQt

--- Sources/Qlift/DispatchQt.swift
+++ Sources/Qlift/DispatchQt.swift
@@ -1,11 +1,16 @@
 //
-//  DispatchQThread.swift
+//  DispatchQt.swift
 //  
 //
 //  Created by Dmitriy Borovikov on 19.05.2021.
 //
 
 import CQlift
+#if os(Linux)
+import Foundation
+import CoreFoundation
+import CDispatch
+#endif
 
 private class ClosureBox {
     public var closure: () -> Void
@@ -25,3 +30,17 @@ public func dispatchQt(block: @escaping () -> Void) {
     let rawClosure = Unmanaged.passRetained(box).toOpaque()
     DispatchToQTMainThread(rawClosure, functor)
 }
+
+#if os(Linux)
+public func bindMainRunloopQt() {
+    let act: CFRunLoopActivity = [.entry]
+    let observer = CFRunLoopObserverCreateWithHandler(kCFAllocatorDefault, act.rawValue, true, 1) {
+        _, _ in
+        dispatchQt {
+            while RunLoop.main.run(mode: .default, before: .distantFuture) {}
+        }
+    }
+    let mainLoop = CFRunLoopGetMain()
+    CFRunLoopAddObserver(mainLoop, observer, kCFRunLoopDefaultMode)
+}
+#endif
