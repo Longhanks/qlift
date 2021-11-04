7573f0936ca38a46c98477710310023aa0f8ac62
Dmitriy Borovikov <dmitry@borovikov.name>
2021-11-04 18:35:15 +0300

    Make BrushStyle enum Pascal case

--- Sources/Qlift/QBrush.swift
+++ Sources/Qlift/QBrush.swift
@@ -10,25 +10,25 @@ import CQlift
 public class QBrush {
 
     public enum BrushStyle: Int32 {
-        case noBrush = 0                    //    No brush pattern.
-        case solidPattern = 1               //    Uniform color.
-        case dense1Pattern = 2              //    Extremely dense brush pattern.
-        case dense2Pattern = 3              //    Very dense brush pattern.
-        case dense3Pattern = 4              //    Somewhat dense brush pattern.
-        case dense4Pattern = 5              //    Half dense brush pattern.
-        case dense5Pattern = 6              //    Somewhat sparse brush pattern.
-        case dense6Pattern = 7              //    Very sparse brush pattern.
-        case dense7Pattern = 8              //    Extremely sparse brush pattern.
-        case horPattern = 9                 //    Horizontal lines.
-        case verPattern = 10                //    Vertical lines.
-        case crossPattern = 11              //    Crossing horizontal and vertical lines.
-        case bDiagPattern = 12              //    Backward diagonal lines.
-        case fDiagPattern = 13              //    Forward diagonal lines.
-        case diagCrossPattern = 14          //    Crossing diagonal lines.
-        case linearGradientPattern = 15     //    Linear gradient (set using a dedicated QBrush constructor).
-        case radialGradientPattern = 16     //    Radial gradient (set using a dedicated QBrush constructor).
-        case conicalGradientPattern = 17    //    Conical gradient (set using a dedicated QBrush constructor).
-        case texturePattern = 24            //    Custom pattern (see QBrush::setTexture()).
+        case NoBrush = 0                    //    No brush pattern.
+        case SolidPattern = 1               //    Uniform color.
+        case Dense1Pattern = 2              //    Extremely dense brush pattern.
+        case Dense2Pattern = 3              //    Very dense brush pattern.
+        case Dense3Pattern = 4              //    Somewhat dense brush pattern.
+        case Dense4Pattern = 5              //    Half dense brush pattern.
+        case Dense5Pattern = 6              //    Somewhat sparse brush pattern.
+        case Dense6Pattern = 7              //    Very sparse brush pattern.
+        case Dense7Pattern = 8              //    Extremely sparse brush pattern.
+        case HorPattern = 9                 //    Horizontal lines.
+        case VerPattern = 10                //    Vertical lines.
+        case CrossPattern = 11              //    Crossing horizontal and vertical lines.
+        case BDiagPattern = 12              //    Backward diagonal lines.
+        case FDiagPattern = 13              //    Forward diagonal lines.
+        case DiagCrossPattern = 14          //    Crossing diagonal lines.
+        case LinearGradientPattern = 15     //    Linear gradient (set using a dedicated QBrush constructor).
+        case RadialGradientPattern = 16     //    Radial gradient (set using a dedicated QBrush constructor).
+        case ConicalGradientPattern = 17    //    Conical gradient (set using a dedicated QBrush constructor).
+        case TexturePattern = 24            //    Custom pattern (see QBrush::setTexture()).
     }
 
     public var ptr: UnsafeMutableRawPointer!
@@ -56,7 +56,7 @@ public class QBrush {
         self.ptr = QBrush_new_color_pixmap(color.ptr, pixmap.ptr)
     }
 
-    public init(color: QColor, style: BrushStyle = .solidPattern) {
+    public init(color: QColor, style: BrushStyle = .SolidPattern) {
         self.ptr = QBrush_new_color_style(color.ptr, style.rawValue)
     }
 
