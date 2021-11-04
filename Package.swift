4dc5eada04ad3008574ce7db197089a68c889791
Dmitriy Borovikov <dmitry@borovikov.name>
2021-11-04 21:33:18 +0300

    Package description refactoring

--- Package.swift
+++ Package.swift
@@ -4,29 +4,14 @@ import PackageDescription
 let package = Package(
     name: "Qlift",
     products: [
-        .executable(
-            name: "qlift-uic",
-            targets: ["qlift-uic"]
-        ),
-        .library(
-            name: "Qlift",
-            targets: ["Qlift"]),
+        .executable(name: "qlift-uic", targets: ["qlift-uic"]),
+        .library(name: "Qlift", targets: ["Qlift"]),
     ],
     targets: [
-        .target(
-            name: "Qlift",
-            dependencies: ["CQlift"],
-            path: "Sources/Qlift"),
-        .target(
-            name: "CQlift",
-            dependencies: [
-                .target(name: "CQt5Widgets")
-            ],
-            path: "Sources/qlift-c-api"),
-        .systemLibrary(
-            name: "CQt5Widgets", pkgConfig: "Qt5Widgets"),
-        .target(
-            name: "qlift-uic")
+        .target(name: "Qlift", dependencies: ["CQlift"]),
+        .target(name: "CQlift", dependencies: ["CQt5Widgets"]),
+        .systemLibrary(name: "CQt5Widgets", pkgConfig: "Qt5Widgets"),
+        .target(name: "qlift-uic")
     ],
     cxxLanguageStandard: .cxx1z
 )
