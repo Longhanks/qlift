7ce9e25288802dd6f5a9b8261a8a2589da36f573
Dmitriy Borovikov <dmitry@borovikov.name>
2022-02-07 21:45:07 +0300

    Move qlift-uic to separate repository.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ import PackageDescription
 let package = Package(
     name: "Qlift",
     products: [
-        .executable(name: "qlift-uic", targets: ["qlift-uic"]),
         .library(name: "Qlift", targets: ["Qlift"]),
     ],
     targets: [
@@ -14,7 +13,6 @@ let package = Package(
                 cxxSettings: [.headerSearchPath("private")]),
         .systemLibrary(name: "CQt5Widgets", pkgConfig: "Qt5Widgets"),
         .systemLibrary(name: "CQt5Quick", pkgConfig: "Qt5Quick"),
-        .target(name: "qlift-uic")
     ],
     cxxLanguageStandard: .cxx1z
 )
